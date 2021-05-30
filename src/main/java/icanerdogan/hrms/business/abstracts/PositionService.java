38083a48d74b9f2e1bc765b36151aa7a848da8ea
icanerdogan <ibrahim4545erdogan@gmail.com>
2021-05-30 14:56:23 +0300

    last

--- src/main/java/icanerdogan/hrms/business/abstracts/PositionService.java
+++
@@ -1,9 +0,0 @@
-package icanerdogan.hrms.business.abstracts;
-
-import icanerdogan.hrms.entities.concretes.Position;
-
-import java.util.List;
-
-public interface PositionService {
-    List<Position> getAll();
-}
